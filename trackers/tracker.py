1a55613782bcf0165295436bf95736c91b937671
Dhanush R Moolemane <152917673+dhanush-r-m@users.noreply.github.com>
2025-07-31 22:35:35 +0530

    Update tracker.py

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -8,6 +8,7 @@ import cv2
 import sys 
 sys.path.append('../')
 from utils import get_center_of_bbox, get_bbox_width, get_foot_position
+import matplotlib.pyplot as plt
 
 class Tracker:
     def __init__(self, model_path):
